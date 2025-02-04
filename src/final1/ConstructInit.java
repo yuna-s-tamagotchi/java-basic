c4ccc3fdd7c1bba9943b21b5c022f11e8c749567
DALLAE <82562349+guswjd8694@users.noreply.github.com>
2025-02-04 15:18:00 +0900

    :sparkles: final

---
+++ src/final1/ConstructInit.java
@@ -0,0 +1,10 @@
+package final1;
+
+public class ConstructInit {
+
+    final int value;    // 필드에 사용할 경우 생성자를 통해 한 번 초기화 가능
+
+    public ConstructInit(int value) {
+        this.value = value;
+    }
+}

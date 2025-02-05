0e93abbaf53d6f714470d9cec000e176975cda11
DALLAE <82562349+guswjd8694@users.noreply.github.com>
2025-02-05 13:40:41 +0900

    :sparkles: 다형성2

---
+++ src/poly/ex5/InterfaceMain.java
@@ -0,0 +1,23 @@
+package poly.ex5;
+
+public class InterfaceMain {
+    public static void main(String[] args) {
+        // 인터페이스 생성 불가 완전 추상임
+        // InterfaceAnimal interfaceAnimal = new InterfaceAnimal();
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Caw caw = new Caw();
+
+        soundAnimal(dog);
+        soundAnimal(cat);
+        soundAnimal(caw);
+    }
+
+    // 동물이 추가되더라도 변하지 않는 부분
+    private static void soundAnimal(InterfaceAnimal animal) {
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+}
